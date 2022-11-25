d45b1836d5fb3f458bc94c7c6ad1fa36c86c5925
Suvodeep Das <suvodeep568@gmail.com>
2022-11-25 23:13:56 +0530

    Add files via upload

---
+++ Apex_data.sql
@@ -0,0 +1,8 @@
+------------Insert--------------
+Insert into customer (customer_id,CUST_FIRST_NAME,CUST_LAST_NAME,
+CUST_STREET_ADDRESS1,CUST_STREET_ADDRESS2,CUST_CITY,CUST_POSTAL_CODE,
+CUST_EMAIL,PHONE_NUMBER1,CREDIT_LIMIT,TAGS)
+values(01,'Shariful','Chowdhury','Ashulia Savar','-','Savar','1401',
+'islamashraful9014@yahoo.com','017178145798',10000,'Repeat Customer');
+
+
